663e4aa2fecd53c200974f0f42b4e034b1494932
marxy <334905044@qq.com>
2017-05-27 03:24:16 +0800

    test index

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,17 +1,27 @@
 <template>
-	<p class="greeting">{{greeting}} World!</p>
+	<div>
+		<p class="greeting">{{greeting}} World!</p>
+		<Input v-model="message"></Input>
+		<p>{{message}}</p>
+		<Button type="ghost" @click="changeMessage">Change</Button>
+	</div>
 </template>
 
 <script>
 	export default{
 		data(){
 			return{
-				greeting:"Hello"
+				greeting:"Hello",
+				message:"",
+			}
+		},
+		methods:{
+			changeMessage(){
+				this.message = "it has been changed!"
 			}
 		}
 	}
 </script>
-
 
 <style scoped>
 	.greeting{
